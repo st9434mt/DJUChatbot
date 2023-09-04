2ccbd37c4b924c86716929fe34e48bdc84dc1fd4
st9434mt <80099308+st9434mt@users.noreply.github.com>
2023-09-04 23:45:40 +0900

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -36,8 +36,8 @@ async function fetchAIResponse(prompt) {
             temperature: 0.8, // 모델의 출력 다양성
             max_tokens: 1024, // 응답받을 메시지 최대 토큰(단어) 수 설정
             top_p: 1, // 토큰 샘플링 확률을 설정
-            frequency_penalty: 0.5, // 일반적으로 나오지 않는 단어를 억제하는 정도
-            presence_penalty: 0.5, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
+            frequency_penalty: 0.8, // 일반적으로 나오지 않는 단어를 억제하는 정도
+            presence_penalty: 0.8, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
             stop: ["Human"], // 생성된 텍스트에서 종료 구문을 설정
         }),
     };
